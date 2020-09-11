15b804328362d7ce5eaa890c8ae418745f51ba1f
chaycesol <chayce.solchaga@gmail.com>
2020-09-11 08:29:27 -0700

    initial project structure

---
+++ api/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const helmet = require('helmet');
+
+
+// **We will comment these back in when we get to a later step
+// const db = require('whatever/db-config.js');
+
+// const whateverRouter = require('./routers/whatever-router.js');
+
+const server = express();
+
+//Third-Party Middleware
+server.use(helmet());
+
+// Node Middleware
+server.use(express.json());
+// server.use('/api/recipes', recipesRouter);
+
+//Custom Middleware
+server.use(logger());
+
+// Base Endpoint
+server.get('/', (req, res) =>{
+    res.status(200).json({ message: "up and running"})
+})
+
+// **We will comment this back in when we get to a later step
+// Router
+// server.use('/api/whatever', whateverRouter)
+
+//Custom Middleware
+function logger(req, res, next) {
+    return function (req, res, next) {
+      console.log(`a ${req.method} request was made to ${req.url} at ${new Date()}`);
+      next();
+    }
+  }
+
+  module.exports = server;
---
+++ queries.sql
@@ -0,0 +1,7 @@
+Build an API with endpoints for:
+ adding resources.
+ retrieving a list of resources.
+ adding projects.
+ retrieving a list of projects.
+ adding tasks.
+ retrieving a list of tasks. The list of tasks should include the project name and project description.
